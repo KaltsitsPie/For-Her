e16a9222acdfef76a4b0cfdce65e5140b0d34e9b
unknown <unknown@example.com>
2021-04-11 22:29:48 +0800

    change structure

--- miniprogram/pages/indexPages/indexRepairer/indexRepairer.js
+++ miniprogram/pages/indexPages/indexRepairer/indexRepairer.js
@@ -1,4 +1,4 @@
-// pages/indexCustomer/indexCustomer.js
+// pages/indexRepairer/indexRepairer.js
 Page({
 
   /**
--- miniprogram/pages/orderPages/allOrder/allOrder.js
+++ miniprogram/pages/orderPages/allOrder/allOrder.js
@@ -1,4 +1,4 @@
-// pages/index/index.js
+// pages/allOrder/allOrder.js
 Page({
 
   /**
