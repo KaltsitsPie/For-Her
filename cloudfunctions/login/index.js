4a813a8a9b8c4658331a4855a10d599741582780
KaltsitsPie <beinu2137@gmail.com>
2021-04-14 16:30:02 +0800

    init: login云函数

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -1,36 +1,156 @@
-// 云函数模板
-// 部署：在 cloud-functions/login 文件夹右击选择 “上传并部署”
-
+// 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-// 初始化 cloud
-cloud.init({
-  // API 调用都保持和云函数当前所在环境一致
-  env: cloud.DYNAMIC_CURRENT_ENV
-})
+cloud.init()
 
 /**
- * 这个示例将经自动鉴权过的小程序用户 openid 返回给小程序端
- * 
- * event 参数包含小程序端调用传入的 data
- * 
+ * 函数内部调用
+ * @param {*} event 
+ *  {
+      "openid": openid,
+      "userInfo" : {
+        "openid": openid,
+        "nickName": nickName,
+        "avatarUrl": avatarUrl
+      }
  */
 exports.main = async (event, context) => {
   console.log(event)
-  console.log(context)
+  // var openid = event.openid
+  //是否新用户
+  var is_new = false
+  var result = {}
+  var user_detail = {}
+  var errCode = 0
+  var errMsg = ""
+  const openid = cloud.getWXContext().OPENID
 
-  // 可执行其他自定义逻辑
-  // console.log 的内容可以在云开发云函数调用日志查看
-
-  // 获取 WX Context (微信调用上下文)，包括 OPENID、APPID、及 UNIONID（需满足 UNIONID 获取条件）等信息
-  const wxContext = cloud.getWXContext()
-
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-    env: wxContext.ENV,
+  //检查参数是否完整
+  if (event.nickName == undefined || event.avatarUrl == undefined) {
+    errCode = 1
+    errMsg = "缺少必要参数"
+    console.log("缺少必要参数，登录失败")
+    result = {
+      "errCode": errCode,
+      "errMsg": errMsg
+    }
+    return result
   }
-}
 
+  //实例化数据库连接并指定环境
+  const db = cloud.database()
+  console.log("数据库连接成功")
+
+  //检查是否新用户
+   const countResult = await db.collection('user_detail')
+    .where({
+        "openid": openid
+      })
+    .count()
+    const total = countResult.total
+    console.log("总共有" + total + "条记录")
+    if (total == 0) {
+      is_new = true
+    }
+
+  //如果是新用户，新增记录
+  if (is_new) {
+    console.log("该用户是新用户，准备新增记录")
+    //需要添加的数据
+    to_add_data = {
+      "openid": openid,
+      "userInfo": {
+        "nickName": event.nickName,
+        "avatarUrl": event.avatarUrl,
+        "openid": openid
+      },
+      "is_black": false,
+      "is_manager": false,
+      "type": 1,
+      "order_quanity": 0,
+      "evaluation_aver": 0,
+      "is_bail": true
+    }
+  
+      //异步调用api，往user-info集合中添加数据
+      await db.collection('user_detail')
+      .add({
+        data: to_add_data
+      })
+      .then(res => {
+        console.log(res)
+        console.log("user_detail新增记录成功")
+        // _id = res._id
+      })
+  }
+  //不是新用户，更新用户信息
+  else {
+    await db.collection('user_detail')
+    .where({
+      "open_id": openid
+    })
+    .update({
+      data: {
+        "userInfo": {
+          "nickName": event.nickName,
+          "avatarUrl": event.avatarUrl,
+          "openid": openid
+        }
+      }
+    })
+    .then(res => {
+      console.log("更新用户信息成功")
+        console.log(res.data)
+    })
+  }
+  
+  //根据openid查询，返回
+  await db.collection('user_detail')
+  .where({
+    "openid": openid
+  })
+  .get()
+  .then(res => {
+   if (res.data.length > 0) {
+     console.log("登录成功，将返回user_detail信息")
+     errCode = 0
+     user_detail = res.data[0]
+   }
+   else {
+    console.log("登录失败")
+    errCode = 2
+    errMsg = "新增用户失败，请重试"
+   }
+ })
+
+ if (errCode != 0) {
+   return {
+    "errCode": errCode,
+    "errMsg": errMsg
+   }
+ }
+ else {
+   if (is_new) {
+     return {
+       "errCode": 0,
+       "errMsg": "",
+       "data": {
+         "is_new": true,
+         "user_detail": user_detail
+       }
+     }
+   }
+   else {
+    return {
+      "errCode": 0,
+      "errMsg": "",
+      "data": {
+        "is_new": false,
+        "user_detail": user_detail
+      }
+    }
+   }
+ }
+// return result
+
+}
