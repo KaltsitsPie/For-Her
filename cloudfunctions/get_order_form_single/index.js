5376643e7e6fc2fd02e8d179fcddc3a67f7633ff
KaltsitsPie <beinu2137@gmail.com>
2021-04-16 21:17:02 +0800

    update: get_order_form_single增加更新订单状态逻辑

--- cloudfunctions/get_order_form_single/index.js
+++ cloudfunctions/get_order_form_single/index.js
@@ -21,6 +21,12 @@ exports.main = async (event, context) => {
     }
   }
 
+  //更新一下订单状态
+  await cloud.callFunction({
+    name: 'check_order_stat_with_time',
+    data: {}
+  })
+  
   const db = cloud.database()
   await db.collection('order_form')
   .where({
@@ -46,63 +52,6 @@ exports.main = async (event, context) => {
      "errMsg": errMsg
    }
  }
-
- //检查订单状态，若现在时间超过了订单开始时间，修改已接单订单为进行中，修改未接单订单为已过时
- if (order_form.start_timestamp < new Date()) {
-   console.log('订单已过时，正在检查状态')
-   if (order_form.order_stat == 0) {
-     console.log('未接单，修改订单状态为已过时')
-       //修改数据
-    await db.collection('order_form')
-    .where({
-      "order_id": order_form.order_id
-    })
-    .update({
-      data: {
-          "order_stat": 11
-      }
-    })
-    .then(res => {
-      console.log("操作成功")
-      //输出修改了多少条数据
-      console.log(res.stats.updated)
-      if (res.stats.updated == 0) {
-        errCode = 2
-        errMsg = "修改失败，该用户可能不存在"
-      }
-      // else {
-      //   updated_num = res.stats.updated
-      // }
-    })
-   }
-   else if (order_form.order_stat == 1) {
-     console.log('已接单，修改订单为进行中')
-    await db.collection('order_form')
-    .where({
-      "order_id": order_form.order_id
-    })
-    .update({
-      data: {
-          "order_stat": 2
-      }
-    })
-    .then(res => {
-      console.log("操作成功")
-      //输出修改了多少条数据
-      console.log(res.stats.updated)
-      if (res.stats.updated == 0) {
-        errCode = 2
-        errMsg = "修改失败，该用户可能不存在"
-      }
-      // else {
-      //   updated_num = res.stats.updated
-      // }
-    })
-   }
-   }
-   else {
-    console.log('订单未过时，将返回')
-   }
  
    
   return {
