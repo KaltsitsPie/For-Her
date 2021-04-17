d9fe3a77a8f51e857c6f4a3fc850904f499fe00b
KaltsitsPie <beinu2137@gmail.com>
2021-04-17 23:36:44 +0800

    update：查看收到所有评价列表

--- cloudfunctions/get_all_evaluation_form/index.js
+++ cloudfunctions/get_all_evaluation_form/index.js
@@ -3,14 +3,76 @@ const cloud = require('wx-server-sdk')
 
 cloud.init()
 
-// 云函数入口函数
+/**
+ * 查看收到的所有评价，openid不填默认是自己的
+ */
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
+  var openid = 0
+  var errCode = 0
+  var errMsg = ""
+  var user_detail = {}
+  var evaluation_array = []
 
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+  if (event.openid == undefined) {
+    openid = wxContext.OPENID
   }
+  else {
+    openid = event.openid
+  }
+
+  //检查用户类型
+  const db = cloud.database()
+  await db.collection('user_detail')
+  .where({
+    "openid": openid
+  })
+  .get()
+  .then(res => {
+    if (res.data.length != 1) {
+      errCode = 17
+      errMsg = "用户信息获取失败"
+    }
+    else {
+      user_detail = res.data[0]
+    }
+  })
+
+  if (errCode != 0) {
+    return {
+      "errCode": errCode,
+      "errMsg": errMsg
+    }
+  }
+
+  if (user_detail.type == 1) {
+    await db.collection('evaluation_form')
+    .where({
+      "maintain_openid": "" + openid
+    })
+    .orderBy('order_id', 'desc')
+    .get()
+    .then(res => {
+      console.log('res=', res)
+      evaluation_array = res.data
+    })
+  }
+  else {
+    await db.collection('evaluation_form')
+    .where({
+      "customer_openid": "" + openid
+    })
+    .orderBy('order_id', 'desc')
+    .get()
+    .then(res => {
+      console.log('res=', res)
+      evaluation_array = res.data
+    })
+  }
+
+    return {
+      "errCode": errCode,
+      "errMsg": errMsg,
+      "data": evaluation_array
+    }
 }
