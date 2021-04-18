2b6ae3c5bb886ec74fc900024e479a5135039fdf
KaltsitsPie <beinu2137@gmail.com>
2021-04-18 15:36:26 +0800

    update: 管理员处理申诉

--- cloudfunctions/reject_complaint/index.js
+++ cloudfunctions/reject_complaint/index.js
@@ -1,16 +1,87 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init()
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
 
-// 云函数入口函数
+/**
+ * 管理员驳回申诉，必要参数是order_id
+ */
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
+  const openid = cloud.getWXContext().OPENID
+  var errCode = 0
+  var errMsg = ""
+
+  if (event.order_id == undefined) {
+    return {
+      "errCode": 1,
+      "errMsg": "缺少必要参数"
+    }
+  }
+  const db = cloud.database()
+
+  //修改申诉状态
+  db.collection('complaint_form')
+  .where({
+    "order_id": "" + event.order_id
+  })
+  .update({
+    data: {
+      "complaint_stat": 1
+    }
+  })
+  .then(res => {
+    if (res.stats.updated != 1) {
+      errCode = 2
+      errMsg = "修改申诉状态失败"
+    }
+  })
+  if (errCode != 0) {
+    return {
+      "errCode": errCode,
+      "errMsg": errMsg
+    }
+  }
+
+  //取订单信息
+  var order_form = {}
+  db.collection('order_form')
+  .where({
+    "order_id": "" + event.order_id
+  })
+  .get()
+  .then(res => {
+    if (res.data.length != 1) {
+      errCode = 3
+      errMsg = "获取订单信息失败，请检查参数"
+    }
+    else {
+      order_form = res.data[0]
+    }
+  })
+
+  if (errCode != 0) {
+    return {
+      "errCode": errCode,
+      "errMsg": errMsg
+    }
+  }
+
+  //恢复原本订单状态
+  const order_stat = order_form.price == undefined ? 2 : 3
+  db.collection('order_form')
+  .where({
+    "order_id": "" + event.order_id
+  })
+  .update({
+    data: {
+      order_stat: order_stat
+    }
+  })
 
   return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+    "errCode": errCode,
+    "errMsg": errMsg
   }
 }
