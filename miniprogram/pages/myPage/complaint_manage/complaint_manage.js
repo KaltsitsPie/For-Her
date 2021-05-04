2d324af95e39235f573d04bf7f39c01c051b8447
Erewhon-Lucy <1663523296@qq.com>
2021-05-04 11:43:41 +0800

    draw complaint_manage

---
+++ miniprogram/pages/myPage/complaint_manage/complaint_manage.js
@@ -0,0 +1,83 @@
+// pages/myPage/complaint_admin/complaint_admin.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    orderNumber: "订单编号",
+    array: ['对方非女性','其他'],
+    index: 0
+  },
+
+  pickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+
+  clickagree: function (params) {
+    console.log("点击同意")
+  },
+
+  clickdisagree: function (params) {
+    console.log("点击驳回")
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
