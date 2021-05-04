78de566d60862dc5b5f9b4084ea3f34071bbfc63
Erewhon-Lucy <1663523296@qq.com>
2021-05-04 18:41:08 +0800

    finish 我的申诉

--- miniprogram/pages/myPage/complaints/complaints.js
+++ miniprogram/pages/myPage/complaints/complaints.js
@@ -1,18 +1,45 @@
 // pages/myPage/complaints/complaints.js
+var app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    complaint_array:[],
+    complaint_type_text:['对方不是女性','其他'],
+    complaint_state_text:['未审核','失败','通过']
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+      console.log("canIUseGetUserProfile改变为true")
+    }
+    wx.cloud.callFunction({
+      name:'get_my_complaint_form',
+      data:{},
+      success: res => {
+        console.log(res)				/*接收后端返回数据*/
+        console.log(res.result.data)
+        console.log(res.result.data.complaint_array)
+        this.setData({
+          complaint_array: res.result.data.complaint_array
+        })
+      },
+      fail: err => {
+        console.error('云函数[get_my_complaint_form]调用失败', err)	/*失败处理*/
+      },
+      complete: () => {
+    
+      }
+    })
   },
 
   /**
